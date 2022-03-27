a078bbcca54488118299fa9a2fc3c06bee8327bd
Kyle Youngblood <kyoungbl21@gmail.com>
2022-03-27 12:56:19 -0400

    Initialized variables, working through math logic

---
+++ exercise/js/app.js
@@ -0,0 +1,124 @@
+
+let problem;
+const maxNum = 10;
+
+
+
+function getRandomNumber(maxNum) {
+    return Math.floor(Math.random() * Math.floor(maxNum));
+  }
+
+/**
+ * Parses the display value into a number (float or int).
+ * @param {String} num 
+ */
+ function parseNumber(num) {
+    return num.includes('.') ? parseFloat(num) : parseInt(num);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let display;
+// let previous = null;
+// let operator = null;
+// let operatorClicked = false;
+
+// /**
+//  * Calculates the operation and updates the display.
+//  */
+// function performOperation() {
+//   let result;
+//   const current = parseNumber(display.value);
+//   previous = parseNumber(previous);
+
+//   switch(operator) {
+//     case '+' :
+//       result = previous + current;
+//     break;
+//     case '-' :
+//         result = previous - current;
+//     break;
+//     case '*' :
+//         result = previous * current;
+//     break;
+//     case '/' :
+//         result = previous / current;
+//     break;
+//   }
+
+//   display.value = result;
+//   operator = null;
+// }
+
+// /**
+//  * Parses the display value into a number (float or int).
+//  * @param {String} num 
+//  */
+// function parseNumber(num) {
+//   return num.includes('.') ? parseFloat(num) : parseInt(num);
+// }
+
+// /**
+//  * Capture the previous value and the clicked operator
+//  * so that an operation can be performed.
+//  */
+// function clickOperator(event) {
+//   operator = event.target.value;
+//   previous = display.value;
+//   operatorClicked = true;
+// }
+
+// /**
+//  * Captures a number click and updates the display value.
+//  * @param {Event} event 
+//  */
+// function clickNumber(event) {
+//   const val = event.target.value;
+
+//   if( operatorClicked ) {
+//     display.value = val;
+//     operatorClicked = false;
+//   } else {
+//     display.value == 0 ? display.value = val : display.value += val;
+//   }
+
+// }
+
+// /**
+//  * Resets the display value.
+//  */
+// function clear() {
+//   display.value = 0;
+// }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+});
